771a4528cc60949a0ebe184e29db65d5ecdca5eb
IgorPustylnik <darthveider36@gmail.com>
2025-04-05 00:52:49 +0300

    [DOM-213] Update validation

--- Domostroy/Domostroy/Library/Base Classes/TextFieldValidator/TextFieldValidator.swift
+++ Domostroy/Domostroy/Library/Base Classes/TextFieldValidator/TextFieldValidator.swift
@@ -15,6 +15,7 @@ enum TextValidator {
     case email
     case phone
     case password
+    case required
 
     func validate(_ text: String?) -> ValidationResult {
         guard let text else {
@@ -30,10 +31,15 @@ enum TextValidator {
             return validatePhone(text)
         case .password:
             return validatePassword(text)
+        case .required:
+            return validateRequired(text)
         }
     }
 
     private func validateUsername(_ text: String) -> ValidationResult {
+        if text.count < 1 {
+            return (false, L10n.Localizable.Auth.InputField.Error.empty)
+        }
         if text.count < 2 {
             return (false, L10n.Localizable.Auth.InputField.Error.Username.short)
         }
@@ -44,6 +50,9 @@ enum TextValidator {
     }
 
     private func validateEmail(_ text: String) -> ValidationResult {
+        if text.count < 1 {
+            return (false, L10n.Localizable.Auth.InputField.Error.empty)
+        }
         let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         if !NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: text) {
             return (false, L10n.Localizable.Auth.InputField.Error.email)
@@ -63,11 +72,45 @@ enum TextValidator {
     }
 
     private func validatePassword(_ text: String) -> ValidationResult {
+        // допустимые символы
+        let allowedCharsRegex = "^[A-Za-z0-9.~!@#$%^&*()+-]+$"
+        let allowedPredicate = NSPredicate(format: "SELF MATCHES %@", allowedCharsRegex)
+        if !allowedPredicate.evaluate(with: text) {
+            return (false, L10n.Localizable.Auth.InputField.Error.Password.invalidSymbols)
+        }
+
+        // хотя бы одна латинская буква
+        let letterPredicate = NSPredicate(format: "SELF MATCHES %@", ".*[A-Za-z]+.*")
+        if !letterPredicate.evaluate(with: text) {
+            return (false, L10n.Localizable.Auth.InputField.Error.Password.noLetter)
+        }
+
+        // хотя бы одна цифра
+        let digitPredicate = NSPredicate(format: "SELF MATCHES %@", ".*[0-9]+.*")
+        if !digitPredicate.evaluate(with: text) {
+            return (false, L10n.Localizable.Auth.InputField.Error.Password.noDigit)
+        }
+
+        // хотя бы один спецсимвол из набора
+        let specialPredicate = NSPredicate(format: "SELF MATCHES %@", ".*[.~!@#$%^&*()+-]+.*")
+        if !specialPredicate.evaluate(with: text) {
+            return (false, L10n.Localizable.Auth.InputField.Error.Password.noSpecialChar)
+        }
+
+        // длина
         if text.count < 8 {
             return (false, L10n.Localizable.Auth.InputField.Error.Password.short)
         }
-        if text.count > 64 {
+        if text.count > 69 {
             return (false, L10n.Localizable.Auth.InputField.Error.Password.long)
+        }
+
+        return (true, nil)
+    }
+
+    private func validateRequired(_ text: String) -> ValidationResult {
+        if text.count < 1 {
+            return (false, L10n.Localizable.Auth.InputField.Error.empty)
         }
         return (true, nil)
     }
--- Domostroy/Domostroy/Resources/Strings/Strings.swift
+++ Domostroy/Domostroy/Resources/Strings/Strings.swift
@@ -31,14 +31,20 @@ internal enum L10n {
           internal enum Password {
             /// Password must contain at least one capital letter
             internal static let capitalletter = L10n.tr("Localizable", "Auth.InputField.Error.Password.capitalletter", fallback: "Password must contain at least one capital letter")
-            /// Password mustn't be longer than 64 characters
-            internal static let long = L10n.tr("Localizable", "Auth.InputField.Error.Password.long", fallback: "Password mustn't be longer than 64 characters")
+            /// Password contains invalid characters
+            internal static let invalidSymbols = L10n.tr("Localizable", "Auth.InputField.Error.Password.invalidSymbols", fallback: "Password contains invalid characters")
+            /// Password mustn't be longer than 69 characters
+            internal static let long = L10n.tr("Localizable", "Auth.InputField.Error.Password.long", fallback: "Password mustn't be longer than 69 characters")
             /// Passwords must match
             internal static let mismatch = L10n.tr("Localizable", "Auth.InputField.Error.Password.mismatch", fallback: "Passwords must match")
-            /// Password mustn't contain non-latin letters
-            internal static let nonlatin = L10n.tr("Localizable", "Auth.InputField.Error.Password.nonlatin", fallback: "Password mustn't contain non-latin letters")
+            /// Password must contain at least one digit
+            internal static let noDigit = L10n.tr("Localizable", "Auth.InputField.Error.Password.noDigit", fallback: "Password must contain at least one digit")
+            /// Password must contain at least one Latin letter
+            internal static let noLetter = L10n.tr("Localizable", "Auth.InputField.Error.Password.noLetter", fallback: "Password must contain at least one Latin letter")
             /// Password must contain at least one number
             internal static let nonumbers = L10n.tr("Localizable", "Auth.InputField.Error.Password.nonumbers", fallback: "Password must contain at least one number")
+            /// Password must contain at least one special character
+            internal static let noSpecialChar = L10n.tr("Localizable", "Auth.InputField.Error.Password.noSpecialChar", fallback: "Password must contain at least one special character")
             /// Password must be at least 8 characters long
             internal static let short = L10n.tr("Localizable", "Auth.InputField.Error.Password.short", fallback: "Password must be at least 8 characters long")
           }
