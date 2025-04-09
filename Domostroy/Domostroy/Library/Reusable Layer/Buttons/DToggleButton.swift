9779702cd5f6310c6e937c921ef6ddc585dee203
IgorPustylnik <darthveider36@gmail.com>
2025-04-09 23:47:16 +0300

    [DOM-214] Add DToggleButton

---
+++ Domostroy/Domostroy/Library/Reusable Layer/Buttons/DToggleButton.swift
@@ -0,0 +1,48 @@
+//
+//  DButtonToggle.swift
+//  Domostroy
+//
+//  Created by Игорь Пустыльник on 09.04.2025.
+//
+
+import UIKit
+
+typealias ToggleAction = (Bool, _ handler: ((_ success: Bool) -> Void)?) -> Void
+
+final class DToggleButton: DButton {
+
+    // MARK: - Properties
+
+    private var isOn: Bool = false
+    private var onImage: UIImage?
+    private var offImage: UIImage?
+    private var toggleAction: ToggleAction?
+
+    // MARK: - Configuration
+
+    func configure(initialState: Bool, onIcon: UIImage?, offIcon: UIImage?, toggleAction: ToggleAction?) {
+        self.onImage = onIcon
+        self.offImage = offIcon
+        self.toggleAction = toggleAction
+        self.actionHandler = { [weak self] in self?.performToggleAction() }
+        updateIcon()
+    }
+
+    // MARK: - Private methods
+
+    private func performToggleAction() {
+        isOn.toggle()
+        updateIcon()
+
+        toggleAction?(isOn) { [weak self] success in
+            if !success {
+                self?.isOn.toggle()
+                self?.updateIcon()
+            }
+        }
+    }
+
+    private func updateIcon() {
+        image = isOn ? onImage : offImage
+    }
+}
