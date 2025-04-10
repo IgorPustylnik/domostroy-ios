2660d67bbb97b7fa5b9d58a3c20fd93a8bfdd732
IgorPustylnik <darthveider36@gmail.com>
2025-04-10 03:19:39 +0300

    [DOM-214] Setuo HomeModule

--- Domostroy/Domostroy/Flows/Home/Home/View/HomeViewController.swift
+++ Domostroy/Domostroy/Flows/Home/Home/View/HomeViewController.swift
@@ -7,24 +7,67 @@
 //
 
 import UIKit
+import SnapKit
+import ReactiveDataDisplayManager
 
 final class HomeViewController: BaseViewController {
 
-    // MARK: - Properties
+    // MARK: - Constants
 
-    private var homeView = HomeView()
+    private enum Constants {
+        static let cellSpacing: CGFloat = 8
+        static let cellHeight: CGFloat = 100
+        static let progressViewHeight: CGFloat = 80
+    }
+
+    // MARK: - UI Elements
+
+    private var activityIndicator = UIActivityIndicatorView(style: .medium)
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+
+    // MARK: - Private Properties
+
+    lazy var progressView = PaginatorView()
+    lazy var refreshControl = UIRefreshControl()
 
     var output: HomeViewOutput?
 
     // MARK: - UIViewController
 
     override func viewDidLoad() {
+        setupCollectionView()
         super.viewDidLoad()
+        configureLayoutFlow()
+        navigationBar.showsMainBar = false
         output?.viewLoaded()
     }
 
-    override func loadView() {
-        view = homeView
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        progressView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: collectionView.frame.width,
+            height: Constants.progressViewHeight
+        )
+    }
+
+    private func setupCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        view.addSubview(activityIndicator)
+        activityIndicator.snp.makeConstraints { $0.center.equalToSuperview() }
+        collectionView.alwaysBounceVertical = true
+        observeScrollOffset(collectionView)
+    }
+
+    func configureLayoutFlow() {
+        let layout = UICollectionViewFlowLayout()
+        let width = UIScreen.main.bounds.width - Constants.cellSpacing * 2
+        layout.itemSize = .init(width: width, height: Constants.cellHeight)
+        collectionView.setCollectionViewLayout(layout, animated: false)
     }
 }
 
@@ -34,6 +77,16 @@ extension HomeViewController: HomeViewInput {
 
     func setupInitialState() {
 
+    }
+
+    func showLoader() {
+        activityIndicator.isHidden = false
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+    }
+
+    func hideLoader() {
+        activityIndicator.stopAnimating()
     }
 
 }
--- Domostroy/Domostroy/Flows/Home/Home/View/HomeViewInput.swift
+++ Domostroy/Domostroy/Flows/Home/Home/View/HomeViewInput.swift
@@ -9,4 +9,6 @@
 protocol HomeViewInput: AnyObject {
     /// Method for setup initial state of view
     func setupInitialState()
+    func showLoader()
+    func hideLoader()
 }
