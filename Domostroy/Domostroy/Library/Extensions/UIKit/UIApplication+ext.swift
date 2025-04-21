9f668b9cc9caa5db70869e49dfbac3cf8adce5c7
IgorPustylnik <darthveider36@gmail.com>
2025-04-21 13:55:06 +0300

    [DOM-239] Fix UIApplication.topViewController

--- Domostroy/Domostroy/Library/Extensions/UIKit/UIApplication+ext.swift
+++ Domostroy/Domostroy/Library/Extensions/UIKit/UIApplication+ext.swift
@@ -13,16 +13,17 @@ extension UIApplication {
             .first(where: { $0.isKeyWindow })
     }
 
-    class func topViewController(_ controller: UIViewController? = {
-        return topWindow()?.rootViewController
-    }()) -> UIViewController? {
+    class func topViewController(
+        _ controller: UIViewController? = topWindow()?.rootViewController
+    ) -> UIViewController? {
         if let navigationController = controller as? UINavigationController {
             return topViewController(navigationController.visibleViewController)
         }
         if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(selected)
-            }
+            return tabController.selectedViewController.flatMap { topViewController($0) }
+        }
+        if let presentedController = controller?.presentedViewController {
+            return topViewController(presentedController)
         }
         return controller
     }
