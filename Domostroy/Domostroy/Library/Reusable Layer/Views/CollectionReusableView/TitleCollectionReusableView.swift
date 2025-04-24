11014514abf98639781d71e754108f7edab6e2c0
IgorPustylnik <darthveider36@gmail.com>
2025-04-25 01:22:53 +0300

    [DOM-265] Change collection title insets

--- Domostroy/Domostroy/Library/Reusable Layer/Views/CollectionReusableView/TitleCollectionReusableView.swift
+++ Domostroy/Domostroy/Library/Reusable Layer/Views/CollectionReusableView/TitleCollectionReusableView.swift
@@ -13,7 +13,7 @@ final class TitleCollectionReusableView: UICollectionReusableView, Accessibility
     // MARK: - Constants
 
     private enum Constants {
-        static let insets: UIEdgeInsets = .init(top: 8, left: 8, bottom: 8, right: 8)
+        static let insets: UIEdgeInsets = .init(top: 8, left: 0, bottom: 8, right: 0)
     }
 
     // MARK: - CalculatableHeightItem
