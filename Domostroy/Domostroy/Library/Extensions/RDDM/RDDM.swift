d938a43c3aa3e4bd099af9c8209f3e5e4dbf97ba
IgorPustylnik <darthveider36@gmail.com>
2025-04-24 22:04:22 +0300

    [DOM-265] Add RDDM diffable resizable generator

--- Domostroy/Domostroy/Library/Extensions/RDDM/RDDM.swift
+++ Domostroy/Domostroy/Library/Extensions/RDDM/RDDM.swift
@@ -117,3 +117,33 @@ extension TitleCollectionHeaderGenerator: DiffableItemSource {
     }
 
 }
+
+// MARK: - DiffableCalculatableHeightCollectionCellGenerator
+
+// Was not originally in RDDM
+
+// swiftlint:disable line_length
+public class DiffableCalculatableHeightCollectionCellGenerator<Cell: ConfigurableItem & CalculatableHeightItem>: DiffableCollectionCellGenerator<Cell> & SizableItem where Cell: UICollectionViewCell, Cell.Model: Equatable {
+// swiftlint:enable line_length
+
+    // MARK: - Private Properties
+
+    private let width: CGFloat
+
+    // MARK: - Initializaion
+
+    public init(uniqueId: AnyHashable,
+                with model: Cell.Model,
+                width: CGFloat,
+                registerType: CellRegisterType = .nib) {
+        self.width = width
+        super.init(uniqueId: uniqueId, with: model, registerType: registerType)
+    }
+
+    // MARK: - SizableCollectionCellGenerator
+
+    public func getSize() -> CGSize {
+        return .init(width: width, height: Cell.getHeight(forWidth: width, with: model))
+    }
+
+}
