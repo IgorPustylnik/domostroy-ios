4b571a248e535360df3403847e3ec392a076034a
IgorPustylnik <darthveider36@gmail.com>
2025-05-05 01:52:27 +0300

    [DOM-293] Add DateFormatter extension

---
+++ Domostroy/Domostroy/Library/Extensions/DateFormatter+ext.swift
@@ -0,0 +1,23 @@
+//
+//  DateFormatter.swift
+//  Domostroy
+//
+//  Created by Игорь Пустыльник on 05.05.2025.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static var iso8601WithMicroseconds: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }
+
+    static var yyyymmdd: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd"
+        return formatter
+    }
+}
